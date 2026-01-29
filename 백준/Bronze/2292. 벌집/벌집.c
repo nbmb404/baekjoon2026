7084282a354f715cc7e4471757fa7304d8204df9
김수지 <nbmb404@gmail.com>
2026-01-29 11:14:04 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.c
+++ 백준/Bronze/2292. 벌집/벌집.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-int main() {
-	int n, i = 1, set = 7, check = 1;
-	scanf("%d", &n);
-	while (1) {
-		if (n == 1) {
-			printf("1");
-			break;
-		}
-		else if (n <= 7) {
-			printf("2");
-			break;
-		}
-		else if (n <= set) {
-			printf("%d", i+1);
-			break;
-		}
-		i++;
-		set += (5 * i) + i;
-	}
-	
+int main(void)
+{
+    int n, i = 2, j = 5, cnt = 2;
+    
+    scanf("%d", &n);
+    if(n == 1)
+    {
+        printf("%d", 1);
+        return 0;
+    }
+    while(1)
+    {
+        if(i + j >= n)
+        {
+            printf("%d", cnt);
+            break;
+        }
+        i = i + j + 1;
+        j += 6;
+        cnt++;
+    }
 }
-
