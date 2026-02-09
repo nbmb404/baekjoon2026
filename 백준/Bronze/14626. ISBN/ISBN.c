5fdcb47b5d4fdf55f12723840134067c058558f9
김수지 <nbmb404@gmail.com>
2026-02-09 16:10:03 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.c
+++ 백준/Bronze/14626. ISBN/ISBN.c
@@ -1,38 +1,22 @@
 #include <stdio.h>
 
 int main() {
-	int p, sum = 0;
+	int p_num, num, sum = 0;
 	char array[14] = { 0 };
 	scanf("%s", array);
 	
-	for (int i = 0; i < 14; i++) if (array[i]!='*') array[i] -= 48;
 	
-
-	for (int i = 1; i < 13; i++) {
+	for (int i = 0; i < 13; i++) {
 		
-		if (array[i - 1] == '*') {
-			p = i;
-			array[i - 1] = 0; // sum 더할 때 착오 없게끔
-		}
+		num = (i % 2 == 0) ? 1 : 3;
+		//num을 곱해야 하기 때문에 1or3
 
-		else if (i % 2 == 0) array[i - 1] *= 3;
-		sum += array[i - 1];
+		if (array[i] == '*') p_num = num;
+		else sum += (array[i] - '0') * num;
 
 	}
 
-	sum += array[12];
-
-	if (p % 2 == 0) {
-		for (int i = 0; i < 10; i++) {
-			if ((sum + (i * 3)) % 10 == 0) printf("%d", i);	
-		}
-	}
-	else {
-		for (int i = 1; i < 10; i++) {
-			if ((sum + i) % 10 == 0) printf("%d", i);
-		
-		}
-	}
+	for (int i = 0; i < 10; i++) if ((sum + i * p_num) % 10 == 0) printf("%d", i);
 
 	return 0;
 }
